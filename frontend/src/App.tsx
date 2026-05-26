bc25ddcfdaab5b00a5d4d9ac0a55420f552334c5
Saad Sayyed <saadsyed950@gmail.com>
2026-05-26 19:49:54 +0530

    ShadCN init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
+  const getToken = async () => {};
+
   return (
-    <div className="flex justify-center items-center w-full min-h-screen">
-      App
+    <div className="flex justify-center items-center w-full min-h-screen flex-col gap-y-6">
+      <Button>Continue with Google</Button>
+      <Button>Logout</Button>
     </div>
   );
 };
