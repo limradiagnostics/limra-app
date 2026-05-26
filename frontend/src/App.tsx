196da8e798c8f38a30e1a6707ca7090227650046
Saad Sayyed <saadsyed950@gmail.com>
2026-05-26 20:07:54 +0530

    Supabase Auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from "react";
 import { Button } from "./components/ui/button";
+import { supabase } from "./lib/supabase";
 
 const App = () => {
-  const getToken = async () => {};
+  const [user, setUser] = useState(null);
+
+  const handleLogin = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+  };
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+  };
+
+  useEffect(() => {
+    const getUser = async () => {
+      const { data, error } = await supabase.auth.getUser();
+
+      if (error) {
+        console.log(error.message);
+        return;
+      }
+
+      console.log(data.user);
+      setUser(data.user);
+    };
+
+    getUser();
+  }, []);
 
   return (
     <div className="flex justify-center items-center w-full min-h-screen flex-col gap-y-6">
-      <Button>Continue with Google</Button>
-      <Button>Logout</Button>
+      {!user && <Button onClick={handleLogin}>Continue with Google</Button>}
+      {user && <Button onClick={handleLogout}>Logout</Button>}
+      {user && <h1>{user?.id!}</h1>}
     </div>
   );
 };
