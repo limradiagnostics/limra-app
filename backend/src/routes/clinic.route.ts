8232127496d3b85ac3147402a65b031edf5da2e9
Saad Sayyed <saadsyed950@gmail.com>
2026-05-27 14:41:12 +0530

    Pre: Fetch clinic profile route

--- backend/src/routes/clinic.route.ts
+++ backend/src/routes/clinic.route.ts
@@ -4,5 +4,6 @@ import * as clinicController from "../controllers/clinic.controller";
 const clinicRouter = Router();
 
 clinicRouter.post("/account/sync", clinicController.syncUserController);
+clinicRouter.get("/account", clinicController.fetchClinicUserController);
 
 export default clinicRouter;
