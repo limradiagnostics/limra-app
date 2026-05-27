94c72cdd6aca85499403487ae0ec53bdc2d9a9a5
Saad Sayyed <saadsyed950@gmail.com>
2026-05-27 14:04:24 +0530

    Sync clinic user controller

---
+++ backend/src/controllers/clinic.controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import * as clinicServices from "../services/clinic.service";
+
+export const syncUserController = async (req: Request, res: Response) => {
+  const { supabaseId, name, email, profilePictureUrl } = req.body;
+  const data = { supabaseId, name, email, profilePictureUrl };
+
+  if (!data) {
+    console.log("Required fields are missing");
+    return res.status(404).json("Required fields are missing");
+  }
+
+  try {
+    const clinic = await clinicServices.syncUserService(
+      supabaseId,
+      name,
+      email,
+      profilePictureUrl,
+    );
+    res.status(201).json({ message: "Supabase clinic account sync", clinic });
+  } catch (error: any) {
+    console.log(error.message);
+    return res.status(500).json(error.message);
+  }
+};
