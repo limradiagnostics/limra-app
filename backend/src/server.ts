e3b5edec3764a1145ea8203e9e35067c6a86d649
Saad Sayyed <saadsyed950@gmail.com>
2026-05-27 13:46:20 +0530

    Clinic account model

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import { connectToDB } from "./config/database.config";
 
 const app = express();
 const PORT = 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on PORT:${PORT}`);
-});
+const startServer = async () => {
+  await connectToDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on PORT:${PORT}`);
+  });
+};
+
+startServer();
