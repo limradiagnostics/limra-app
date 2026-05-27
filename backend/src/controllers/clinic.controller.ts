012348d326faa352d4360e331b1f55c8e9871b81
Saad Sayyed <saadsyed950@gmail.com>
2026-05-27 14:40:40 +0530

    Pre: Fetch clinic profile controller

--- backend/src/controllers/clinic.controller.ts
+++ backend/src/controllers/clinic.controller.ts
@@ -23,3 +23,25 @@ export const syncUserController = async (req: Request, res: Response) => {
     return res.status(500).json(error.message);
   }
 };
+
+export const fetchClinicUserController = async (
+  req: Request,
+  res: Response,
+) => {
+  const { supabaseId } = req.params;
+
+  if (!supabaseId) {
+    console.log("Provide supabase ID");
+    return res.status(404).json("Provide supabase ID");
+  }
+
+  try {
+    const clinic = await clinicServices.fetchClinicUserService(
+      supabaseId as string,
+    );
+    res.status(200).json({ clinic });
+  } catch (error: any) {
+    console.log(error.message);
+    return res.status(400).json(error.message);
+  }
+};
