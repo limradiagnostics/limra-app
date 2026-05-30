ba2791bb552081e9c410c12150514bdf62e0b782
Saad Sayyed <saadsyed950@gmail.com>
2026-05-30 19:45:53 +0530

    Generate bill service

---
+++ backend/src/services/bill.service.ts
@@ -0,0 +1,30 @@
+import { ScanType } from "@prisma/client";
+import prisma from "../lib/prisma.orm";
+
+export const generateBillService = async (
+  patientId: string,
+  scanType: ScanType,
+  totalAmount: number,
+  concession: number,
+  dueAmount: number,
+) => {
+  const patient = await prisma.patient.findUnique({
+    where: {
+      id: patientId,
+    },
+  });
+
+  const bill = await prisma.bill.create({
+    data: {
+      patientId,
+      scanType,
+      totalAmount,
+      concession,
+      dueAmount,
+    },
+  });
+
+  const patientName = patient?.name;
+
+  return { patientName, bill };
+};
