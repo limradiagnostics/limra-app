49fee9c9dc412a1d805f98333b34e2acec53c0b5
Saad Sayyed <saadsyed950@gmail.com>
2026-05-30 18:30:24 +0530

    Register OBS patient service

--- backend/src/services/patient.service.ts
+++ backend/src/services/patient.service.ts
@@ -30,3 +30,44 @@ export const registerRegularPatientService = async (
 
   return patient;
 };
+
+export const registerOBSPatientService = async (
+  name: string,
+  address: {
+    home: string;
+    localArea: string;
+    pincode: number;
+    city: string;
+    state: string;
+  },
+  phone: number,
+  aadharNumber: number,
+  progenies: [
+    {
+      gender: string;
+      age: number;
+    },
+  ],
+  husbandName: string,
+) => {
+  const existing = await prisma.patient.findUnique({
+    where: {
+      phone,
+    },
+  });
+  if (existing) throw new Error("Patient account already exists");
+
+  const patient = await prisma.patient.create({
+    data: {
+      name,
+      address,
+      phone,
+      aadharNumber,
+      progenies,
+      husbandName,
+      type: PatientType.OBS,
+    },
+  });
+
+  return patient;
+};
